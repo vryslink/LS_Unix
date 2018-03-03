36663816d08dee94db79847b45ad64761e5a7bbe
Vaclav Ryslink <ryslinkv@u-pl0.ms.mff.cuni.cz>
2018-03-03 15:13:22 +0100

    oprava ukolu

---
+++ u02/program.pas
@@ -0,0 +1,9 @@
+program Nic;
+var i : integer;
+begin
+for i:=1 to 5 do writeln ('Ahoj');
+for i:=1 to 5 do writeln ('.,-{}][\;;;');
+for i:=1 to 5 do writeln (i);
+for i:=1 to 5 do writeln (' ');
+for i:=1 to 10 do writeln (i, '  ', i);
+end.
